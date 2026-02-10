2030a1557e036bb224776fd053b730e2e17f2c39
Lee HaYeong <dlgkdud4759@naver.com>
2026-02-10 11:20:36 +0900

    📝 Update: 프로필 이미지 수정

--- components/ui/Chatting.tsx
+++ components/ui/Chatting.tsx
@@ -7,20 +7,19 @@ interface MessageBubbleProps {
   isMine: boolean; // 본인이 보낸 메시지인지 여부
   sender?: User; // 메시지 발신인 정보 (상대방 메시지인 경우에만 주로 사용)
   currentUser?: User; // 현재 로그인한 사용자 정보 추가
+  showTimestamp?: boolean; // 시간 표시 여부 추가
 }
 
 export default function Chatting({
   message,
   isMine,
   sender,
-  currentUser,
 }: MessageBubbleProps) {
   // 본인 메시지면 currentUser의 이미지, 상대방 메시지면 sender의 이미지
-  const avatarSrc = isMine
-    ? currentUser?.image || '/images/Logo.png'
-    : sender?.image || '/images/Logo.png';
+  const avatarSrc =
+    !isMine && sender?.image ? sender.image : '/images/Logo.png';
 
-  const wrapperCls = isMine ? 'flex-row-reverse' : 'flex';
+  const wrapperCls = isMine ? 'flex-row-reverse mr-2' : 'flex';
   const bubbleCls = isMine
     ? 'rounded-tl-xl rounded-b-xl bg-brown-accent text-white'
     : 'rounded-tr-xl rounded-b-xl bg-brown-guide text-white';
@@ -38,13 +37,15 @@ export default function Chatting({
   return (
     <>
       <article className={`flex ${wrapperCls}`}>
-        <Image
-          className="w-10 h-10 m-2 bg-border-primary rounded-md"
-          alt="사용자 프로필"
-          src={`${avatarSrc}`}
-          width={40}
-          height={40}
-        />
+        {!isMine && (
+          <Image
+            className="w-10 h-10 m-2 bg-border-primary rounded-md"
+            alt="사용자 프로필"
+            src={avatarSrc}
+            width={40}
+            height={40}
+          />
+        )}
 
         <div className="flex gap-2 items-end">
           {/* 메타 정보: 내 메시지면 앞에, 상대 메시지면 뒤에 렌더링 용도 */}
